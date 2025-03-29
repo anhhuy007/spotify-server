bcad516753ad674e804edca4767a8590c6343c1c
Duyzhii <duyzhii@gmail.com>
2025-03-29 16:46:34 +0700

    fix: replace assert import with readFileSync for better compatibility on Render

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,8 +1,15 @@
-import admin from 'firebase-admin';
-import serviceAccount from '../secrets/spotify-clone-c39ee-firebase-adminsdk-fbsvc-7658bfc9e2.json' assert { type: 'json' };
+import fs from "fs";
+import path from "path";
+import admin from "firebase-admin";
+
+const serviceAccountPath = path.resolve(
+  "secrets",
+  "spotify-clone-c39ee-firebase-adminsdk-fbsvc-7658bfc9e2.json"
+);
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
