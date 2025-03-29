7a297772b9aa73ee7933a4ad778bf67d6ea9d914
Duyzhii <duyzhii@gmail.com>
2025-03-29 16:51:57 +0700

    fix: replace JSON file import with env variable for Firebase credentials

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,12 +1,6 @@
-import fs from "fs";
-import path from "path";
 import admin from "firebase-admin";
 
-const serviceAccountPath = path.resolve(
-  "secrets",
-  "spotify-clone-c39ee-firebase-adminsdk-fbsvc-7658bfc9e2.json"
-);
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
