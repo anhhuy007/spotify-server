02309d4fab6b55cf8ef93d653e68d7a0308c6f78
nhatphuoc <vonhatphuoc32@gmail.com>
2025-04-02 14:29:17 +0700

    feat: follow, fix: top song

---
+++ routes/follower.route.js
@@ -0,0 +1,34 @@
+import express from "express";
+import followerController from "../controllers/follower.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.post(
+  "/check",
+  authMiddleware.authenticateUser,
+  followerController.checkFollowerExists
+);
+
+router.post(
+  "/add",
+  authMiddleware.authenticateUser,
+  followerController.addFollower
+);
+router.get(
+  "/:user_id/list",
+  authMiddleware.authenticateUser,
+  followerController.getFollowedArtists
+);
+router.get(
+  "/:user_id/count",
+  authMiddleware.authenticateUser,
+  followerController.getCountFollowedArtists
+);
+router.delete(
+  "/delete/:id",
+  authMiddleware.authenticateUser,
+  followerController.deleteFollower
+);
+
+export default router;
