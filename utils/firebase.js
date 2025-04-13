62a079bee06d2203672e560f31b159133b736bdf
Duyzhii <duyzhii@gmail.com>
2025-04-13 20:44:31 +0700

    action: resolve conflict

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import admin from "firebase-admin";
 import fs from "fs";
 import path from "path";
@@ -16,21 +15,6 @@ const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-=======
-import admin from 'firebase-admin';
-import fs from "fs";
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    "./secrets/spotify-clone-c39ee-firebase-adminsdk-fbsvc-7658bfc9e2.json",
-    "utf8"
-  )
-);
-
-
-admin.initializeApp({
-     credential: admin.credential.cert(serviceAccount),
->>>>>>> 8a0c35c951c4a4f9514628c4b63266fd7d4d6094
 });
 
 export default admin;
