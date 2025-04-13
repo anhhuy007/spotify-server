b6ec488513be418bfe9778771179ccccb6a3c2cc
Duyzhii <duyzhii@gmail.com>
2025-04-13 15:14:37 +0700

    fix: fix secrets file path to deploy

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,12 +1,17 @@
 import admin from "firebase-admin";
 import fs from "fs";
+import path from "path";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    "etc/secrets/spotify-clone-c39ee-firebase-adminsdk-fbsvc-7658bfc9e2.json",
-    "utf8"
-  )
-);
+const isProduction = process.env.NODE_ENV === "production";
+
+const serviceAccountPath = isProduction
+  ? "/etc/secrets/spotify-clone-c39ee-firebase-adminsdk-fbsvc-7658bfc9e2.json"
+  : path.join(
+      process.cwd(),
+      "secrets/spotify-clone-c39ee-firebase-adminsdk-fbsvc-7658bfc9e2.json"
+    );
+
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
